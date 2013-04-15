2d21afb1a317bec6a4e7493f0afaac255b5f8cfe
Rob McBroom <github@skurfer.com>
2013-04-15 10:33:56 -0400

    add UI for preferences

---
+++ QSCloudApp/QSCloudPrefPane.h
@@ -0,0 +1,12 @@
+//
+//  QSCloudPrefPane.h
+//  QSCloudApp
+//
+//  Created by Rob McBroom on 2013/04/12.
+//
+
+#import <QSInterface/QSInterface.h>
+
+@interface QSCloudPrefPane : QSPreferencePane
+
+@end
---
+++ QSCloudApp/QSCloudPrefPane.m
@@ -0,0 +1,12 @@
+//
+//  QSCloudPrefPane.m
+//  QSCloudApp
+//
+//  Created by Rob McBroom on 2013/04/12.
+//
+
+#import "QSCloudPrefPane.h"
+
+@implementation QSCloudPrefPane
+
+@end
